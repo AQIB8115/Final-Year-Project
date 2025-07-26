d27877d32c0d8c38c69e67e45fbcb75516cfce09
AQIB8115 <aqibzaman.5042@gmail.com>
2025-07-26 17:59:38 +0500

    Update SuperAdminLogin.js

--- frontend/src/pages/SuperAdminLogin.js
+++ frontend/src/pages/SuperAdminLogin.js
@@ -19,7 +19,7 @@ const SuperAdminLogin = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/login-superadmin', {
+      const response = await fetch('https://final-year-project-api-ten.vercel.app/api/users/login-superadmin', {
         method: 'POST',
         // withCredentials: true,
         headers: {
