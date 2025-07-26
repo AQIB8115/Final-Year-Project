cf99b7e949747938e668fa2e93560f0df952daf5
AQIB8115 <aqibzaman.5042@gmail.com>
2025-07-26 17:55:26 +0500

    Update ReserveTime.js

--- frontend/src/pages/ReserveTime.js
+++ frontend/src/pages/ReserveTime.js
@@ -14,7 +14,7 @@ const ReserveTime = () => {
   useEffect(() => {
     const fetchReservationStatus = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/api/reservation-status/${id}`);
+        const response = await fetch(`https://final-year-project-api-ten.vercel.app/api/reservation-status/${id}`);
         const data = await response.json();
         if (data.status === 'reserved') {
           setIsReserved(true);
@@ -61,7 +61,7 @@ const ReserveTime = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/api/create', {
+      const response = await fetch('https://final-year-project-api-ten.vercel.app/api/create', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(reservationDetails),
