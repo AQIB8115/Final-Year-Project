892fc90c7ec52b0664de886d70753654e96ab715
AQIB8115 <aqibzaman.5042@gmail.com>
2025-07-26 17:46:23 +0500

    Update CreateBook.js

--- frontend/src/pages/CreateBook.js
+++ frontend/src/pages/CreateBook.js
@@ -50,7 +50,7 @@ const CreateBook = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5000/api/books', {
+            const response = await fetch('https://final-year-project-api-ten.vercel.app/api/books', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
